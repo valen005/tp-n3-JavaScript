a1342b3cd31d62d8ff67c8c47b92469afec7dde6
Valentina Acosta <valuchi005@gmail.com>
2023-11-14 17:11:10 -0300

    Logre 2 numeros randoms y sumarlos

--- js/03-ejercicio.js
+++ js/03-ejercicio.js
@@ -2,3 +2,11 @@
 para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado 
 de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.
  */
+let dado1 = Math.floor(Math.random() * 6) + 1;
+let dado2= Math.floor(Math.random() * 6) + 1;
+
+console.log(`Dado 1: ${dado1}`);
+console.log(`Dado 2: ${dado2}`);
+
+const suma = dado1 + dado2
+console.log (suma)
