a86fe37bb8126ff26e8a4edc37ccaa9139d03b48
Valentina Acosta <valuchi005@gmail.com>
2023-11-14 17:54:51 -0300

    terminado punto 5

--- js/05-ejercicio.js
+++ js/05-ejercicio.js
@@ -2,3 +2,17 @@
 se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si
  esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
  */
+
+ const frase = prompt('Introduce una frase:')
+
+ function analizarCadena(cadena) {
+   
+    if (cadena === cadena.toUpperCase()) {
+        document.write("La cadena está formada solo por mayúsculas.");
+    } else if (cadena === cadena.toLowerCase()) {
+        document.write("La cadena está formada solo por minúsculas.");
+    } else {
+        document.write("La cadena es una mezcla de mayúsculas y minúsculas.");
+    }
+}
+let resultado = analizarCadena(frase);
